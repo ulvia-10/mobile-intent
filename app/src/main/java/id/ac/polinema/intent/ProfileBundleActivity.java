496692b68f42a113b747da423b652c91a35aed2a
ulvia <ulvia.yulianti@gmail.com>
2020-02-20 21:01:20 +0700

    merubah pada android bundel, explicit,dan implicit intent pada pertemuan 3

--- app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -1,21 +1,36 @@
 package id.ac.polinema.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileBundleActivity extends AppCompatActivity {
-
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_bundle);
 
         // TODO: bind here
-
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             // TODO: display value here
+            String username = extras.getString(BundleActivity.USERNAME_KEY);
+            String name  = extras.getString(BundleActivity.NAME_KEY);
+            int age = extras.getInt(BundleActivity.AGE_KEY);
+            usernameText.setText(username);
+            nameText.setText(name);
+            ageText.setText(String.valueOf(age));
         }
+
+
     }
+
 }
